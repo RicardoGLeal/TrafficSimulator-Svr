8ebfdcd56cc474c5aa5cc18149f7cafd467c36d7
Carlo <carlouxljlj@gmail.com>
2023-01-26 12:00:56 -0600

    conexiones repair choques

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 mapa = pd.read_csv("grafo.csv", header= None).values
-conexiones = pd.read_csv("conexiones14.csv", index_col = [0]).to_dict("index")
+conexiones = pd.read_csv("conexiones15.csv", index_col = [0]).to_dict("index")
